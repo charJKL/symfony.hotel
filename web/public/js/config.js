98566848675b8d86e1a55eac53cbc4548b7defe9
charJKL <11600464+charJKL@users.noreply.github.com>
2021-10-07 12:00:15 +0200

    Retrive /api/rooms from `web` client.

---
+++ web/public/js/config.js
@@ -0,0 +1,1 @@
+export const API_URL_HOST = 'http://localhost:8000';
--- web/public/js/main/homepage.js
+++ web/public/js/main/homepage.js
@@ -1,5 +1,24 @@
+import {API_URL_HOST} from '/js/config.js';
+
 document.addEventListener("DOMContentLoaded", function(event) 
 {
 	console.log("js/main/homepage.js");
+	
+	console.log(parseFloat("351.45"));
+	
+	const headers = new Headers();
+			headers.set('Accept', 'application/json');
+	const request = fetch(API_URL_HOST + '/api/rooms', {method: 'GET', headers});
+	const response = request.then(onResponse).then(onData);
 });
 
+function onResponse(response)
+{
+	console.log(response);
+	return response.json();
+}
+
+function onData(data)
+{
+	console.log(data);
+}
