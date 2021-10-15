515b601db98b688ef6bffeafd3c0e7aa84a18578
charJKL <11600464+charJKL@users.noreply.github.com>
2021-10-15 15:52:26 +0200

    Run test case against database.

--- api/src/Test/ApiTestCase.php
+++ api/src/Test/ApiTestCase.php
@@ -3,18 +3,23 @@ namespace App\Test;
 
 use Symfony\Component\HttpFoundation\Response;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase as ApiPlatformTestCase;
+use Doctrine\ORM\EntityManagerInterface;
 
 abstract class ApiTestCase extends ApiPlatformTestCase
 {
 	const HTTP_200_OK = Response::HTTP_OK;
 	const HTTP_201_HTTP_CREATED = Response::HTTP_CREATED;
+	const HTTP_404_NOT_FOUND = Response::HTTP_NOT_FOUND;
 	
 	const HEADER_CONTENT_TYPE = 'content-type';
 	const HEADER_ACCEPT = 'accept';
 	
+	const GET = 'GET';
 	const POST = 'POST';
 	const POST_JSON = 'POST+JSON';
-	const GET = 'GET';
+	const PUT = 'PUT';
+	const DELETE = 'DELETE';
+	const PATCH = 'PATCH';
 	
 	protected function request(string $method, string $uri, array $headers = [], array $data = [])
 	{
@@ -22,4 +27,10 @@ abstract class ApiTestCase extends ApiPlatformTestCase
 		if($method === self::POST) return static::createClient()->request(self::POST, $uri, $headers + ['body' => $data]);
 		return static::createClient()->request($method, $uri, $headers + $data);
 	}
+	
+	protected function em(string $repository)
+	{
+		return static::getContainer()->get(EntityManagerInterface::class)->getRepository($repository);
+	}
+	
 }
--- api/tests/apis/AccommodationTest.php
+++ api/tests/apis/AccommodationTest.php
@@ -2,17 +2,21 @@
 namespace Tests\Apis;
 
 use App\Test\ApiTestCase;
+use App\Entity\Accommodation;
 
 class AccommodationTest extends ApiTestCase
 {
 	public function testOnlyEmailOrPhoneIsRequiredToMakeReservation()
 	{
 		$json = ['checkInAt'=>'16.10.2021', 'checkOutAt'=>'19.10.2021', 'email'=>'fake@gmail.com', 'roomsAmount'=> 1, 'peopleAmount'=> 2];
-		$response = $this->request(self::POST_JSON, '/api/accommodations', [], $json);
+		$this->request(self::POST_JSON, '/api/accommodations', [], $json);
 		$this->assertResponseStatusCodeSame(self::HTTP_201_HTTP_CREATED);
 		
+		$accommodations = $this->em(Accommodation::class)->findAll();
+		$this->assertCount(1, $accommodations);
+		
 		$json = ['checkInAt'=>'16.10.2021 12:23', 'checkOutAt'=>'21.10.2021 16:20', 'phone' => '1234-4566-45', 'roomsAmount'=> 2, 'peopleAmount'=> 2];
-		$response = $this->request(self::POST_JSON, 'api/accommodations', [], $json);
+		$this->request(self::POST_JSON, 'api/accommodations', [], $json);
 		$this->assertResponseStatusCodeSame(self::HTTP_201_HTTP_CREATED);
 	}
 }
