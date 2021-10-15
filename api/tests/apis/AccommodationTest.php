6fa833f4faa0ef45852111bfb8f8a2e8b28af0b7
charJKL <11600464+charJKL@users.noreply.github.com>
2021-10-15 16:08:55 +0200

    Using @dataProvider in test case.

--- api/tests/apis/AccommodationTest.php
+++ api/tests/apis/AccommodationTest.php
@@ -6,17 +6,22 @@ use App\Entity\Accommodation;
 
 class AccommodationTest extends ApiTestCase
 {
-	public function testOnlyEmailOrPhoneIsRequiredToMakeReservation()
+	public function emailOrPhoneProvider() : iterable
 	{
-		$json = ['checkInAt'=>'16.10.2021', 'checkOutAt'=>'19.10.2021', 'email'=>'fake@gmail.com', 'roomsAmount'=> 1, 'peopleAmount'=> 2];
+		yield [['checkInAt'=>'16.10.2021', 'checkOutAt'=>'19.10.2021', 'email'=>'fake@gmail.com', 'roomsAmount'=> 1, 'peopleAmount'=> 2]];
+		yield [['checkInAt'=>'16.10.2021 12:23', 'checkOutAt'=>'21.10.2021 16:20', 'phone' => '1234-4566-45', 'roomsAmount'=> 2, 'peopleAmount'=> 2]];
+	}
+	
+	/**
+	 * @dataProvider emailOrPhoneProvider
+	 */
+	public function testOnlyEmailOrPhoneIsRequiredToMakeReservation(array $json)
+	{
 		$this->request(self::POST_JSON, '/api/accommodations', [], $json);
 		$this->assertResponseStatusCodeSame(self::HTTP_201_HTTP_CREATED);
 		
 		$accommodations = $this->em(Accommodation::class)->findAll();
 		$this->assertCount(1, $accommodations);
-		
-		$json = ['checkInAt'=>'16.10.2021 12:23', 'checkOutAt'=>'21.10.2021 16:20', 'phone' => '1234-4566-45', 'roomsAmount'=> 2, 'peopleAmount'=> 2];
-		$this->request(self::POST_JSON, 'api/accommodations', [], $json);
-		$this->assertResponseStatusCodeSame(self::HTTP_201_HTTP_CREATED);
+		$this->assertEquals($accommodations[0]->getStatus(), Accommodation::BOOKED);
 	}
 }
