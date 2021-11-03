596f5ee3ed536ffed6e27dda2f533520e0336290
charJKL <11600464+charJKL@users.noreply.github.com>
2021-11-03 12:11:39 +0100

    Rename `GuestTest` to `LoginAuthenticatonTest`.

--- api/tests/api/LoginAuthenticatonTest.php
+++ api/tests/api/LoginAuthenticatonTest.php
@@ -5,18 +5,26 @@ use App\Test\ApiTestCase;
 use App\Test\ApiClientInterface as http;
 use App\Factory\EmployeeFactory;
 use App\Factory\GuestFactory;
-use App\Entity\Accommodation;
-use App\Entity\Guest;
 
-
-class GuestTest extends ApiTestCase
+class LoginAuthenticatonTest extends ApiTestCase
 {
+	public function testEmployeeCanLogIn()
+	{
+		self::bootKernel();
+		$employee = EmployeeFactory::new()->withUuid('mark.admin')->withPlainPassword('admin-super-password')->create();
+		
+		$json = ['login' => 'mark.admin', 'password' => 'admin-super-password'];
+		
+		$this->request(http::POST, '/login', [], $json);
+		$this->assertResponseStatusCodeSame(http::HTTP_200_OK);
+	}
+	
 	public function testGuestCanLogInByEmail()
 	{
 		self::bootKernel();
 		$guest = GuestFactory::new()->withEmail('fake@email.com')->withPlainPassword('secretPassword')->create();
 
-		$json = ['login' => $guest->getEmail(), 'password' => 'secretPassword'];
+		$json = ['login' => 'fake@email.com', 'password' => 'secretPassword'];
 		
 		$this->request(http::POST, '/login', [], $json);
 		$this->assertResponseStatusCodeSame(http::HTTP_200_OK);
