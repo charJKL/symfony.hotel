458c7faf2996a96f8023f8b1f5ba6e71e5e35995
charJKL <11600464+charJKL@users.noreply.github.com>
2021-11-05 08:56:52 +0100

    Add `Task` and `Service` resources.

---
+++ api/src/Entity/Task.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\TaskRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+#[ApiResource]
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createTime;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $endTime;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Service::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $service;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Room::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $room;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Employee::class)
+     */
+    private $employee;
+
+    public function __construct()
+    {
+        $this->employee = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreateTime(): ?\DateTimeInterface
+    {
+        return $this->createTime;
+    }
+
+    public function setCreateTime(\DateTimeInterface $createTime): self
+    {
+        $this->createTime = $createTime;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->endTime;
+    }
+
+    public function setEndTime(?\DateTimeInterface $endTime): self
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+
+    public function getService(): ?Service
+    {
+        return $this->service;
+    }
+
+    public function setService(Service $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function getRoom(): ?Room
+    {
+        return $this->room;
+    }
+
+    public function setRoom(?Room $room): self
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Employee[]
+     */
+    public function getEmployee(): Collection
+    {
+        return $this->employee;
+    }
+
+    public function addEmployee(Employee $employee): self
+    {
+        if (!$this->employee->contains($employee)) {
+            $this->employee[] = $employee;
+        }
+
+        return $this;
+    }
+
+    public function removeEmployee(Employee $employee): self
+    {
+        $this->employee->removeElement($employee);
+
+        return $this;
+    }
+}
