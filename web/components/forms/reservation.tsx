1797e5d56f4fb29a14f61dd3950acfa973a501ee
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-18 18:11:35 +0100

    Implement ajax request to save reservation.

--- web/components/forms/reservation.tsx
+++ web/components/forms/reservation.tsx
@@ -1,19 +1,39 @@
 import { useFormik } from "formik";
+import axios from 'axios';
 
+type FormInputs =
+{
+	peopleAmount: number;
+	roomAmount: number;
+	contact: string;
+	checkInAt: string;
+	checkOutAt: string;
+}
 
 const Reservation = () : JSX.Element => {
 	const form = useFormik({
-		
+		initialValues: {
+			peopleAmount: 0,
+			roomsAmount: 0,
+			contact: "",
+			checkInAt: "",
+			checkOutAt: "",
+		},
+		onSubmit: (values) => {
+			console.log(values);
+			axios.post("http://localhost:8000/api/reservations", values)
+				.then((r) => { console.log(r); })
+				.catch((r) => { console.error(r); })
+		}
 	});
 	
 	return (
-		<form>
-			<input name="peopleAmount" type="number"/>
-			<input name="roomAmount" type="number"/>
-			<input name="contact" type="text"/>
-			<input name="checkInAt" type="date"/>
-			<input name="checkInAt" type="date"/>
-			
+		<form onSubmit={form.handleSubmit}>
+			<input name="peopleAmount" type="number" onChange={form.handleChange} value={form.values.peopleAmount} />
+			<input name="roomsAmount" type="number" onChange={form.handleChange} value={form.values.roomsAmount} />
+			<input name="contact" type="text" onChange={form.handleChange} value={form.values.contact} />
+			<input name="checkInAt" type="date" onChange={form.handleChange} value={form.values.checkInAt} />
+			<input name="checkOutAt" type="date" onChange={form.handleChange} value={form.values.checkOutAt} />
 			<input type="submit" />
 		</form>
 	)
