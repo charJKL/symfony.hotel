b333e1fea51710f804e3614a773c4700831bb9b0
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-26 13:52:35 +0100

    Hack with redeclaring `forwardRef` to allow generic type.

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -4,6 +4,15 @@ import type { AppProps } from 'next/app';
 import LayoutBase from '../components/layoutBase';
 import AuthenticationContext, {AuthenticationContextData} from '../services/AuthenticationContext';
 
+
+// Redecalare forwardRef
+// https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forward_and_create_ref#option-2---redeclare-forwardref
+declare module "react" {
+  function forwardRef<T, P = {}>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactElement | null
+  ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
 	const [isAuthenticated, setAuthenticated] = useState<AuthenticationContextData>(false);
 	const AuthenticationContextValue = {isAuthenticated, setAuthenticated};
