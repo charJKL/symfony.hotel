3b9d2b8258ded4edb0056822362c87ec0bfff062
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-26 13:53:05 +0100

    Extract `Input` component to separate file.

--- web/components/forms/reservation.tsx
+++ web/components/forms/reservation.tsx
@@ -1,10 +1,9 @@
-import React, { InputHTMLAttributes, useState} from "react";
-import { ForwardedRef } from "react";
-import { forwardRef } from "react";
-import { useForm, SubmitHandler, UseFormRegister, FieldError } from "react-hook-form";
+import React, { useState} from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import instance from "../../axios";
 import styles from "./reservation.module.scss";
 import Modal from "../ui/modal";
+import Input from "../ui/input";
 
 type ReservationInputs =
 {
@@ -15,12 +14,6 @@ type ReservationInputs =
 	checkOutAt: Date | null;
 }
 
-type InputPropsBase = 
-{
-	label?: string;
-	invalid?: FieldError;
-}
-type InputProps = InputPropsBase & InputHTMLAttributes<HTMLInputElement>;
 type FormStatus = 
 {
 	status: "idle" | "waiting" | "error" | "saved";
@@ -72,21 +65,6 @@ const Reservation = () : JSX.Element =>
 		setForm({status: "idle"});
 	}
 	
-	type InputRefType = InputProps & ReturnType<UseFormRegister<ReservationInputs>>
-	const Input = ({className, label, onChange, onBlur, invalid, ...props}: InputRefType, ref: ForwardedRef<HTMLInputElement> ): JSX.Element =>
-	{
-		const isInvalid = invalid != undefined ? styles.invalid : '';
-		const text = invalid != undefined ? invalid.message : label;
-		const styleDiv = [styles.inputDiv, className, isInvalid].join(' ');
-		const styleLabel = [styles.inputLabel, isInvalid].join(' ');
-		const styleInput = [styles.inputInput, isInvalid].join(' ');
-		return (<div className={styleDiv}>
-			<label className={styleLabel}>{text}</label>
-			<input className={styleInput} {...props} ref={ref} onChange={onChange} onBlur={onBlur} />
-		</div>);
-	}
-	const InputRef = forwardRef<HTMLInputElement, InputRefType>(Input);
-	
 	const Button = () : JSX.Element =>
 	{
 		switch(form.status)
@@ -104,11 +82,11 @@ const Reservation = () : JSX.Element =>
 				<fieldset className={styles.fields}>
 					<Modal content={form.detail} visible={form.status == "error"} onClose={disposeModalHandle}/>
 					<Modal content="Twoja rezerwacja została zapisana" visible={form.status == "saved"} onClose={disposeModalHandle} />
-					<InputRef className={styles.peopleAmount} type="number" min="1" label="Ilość osób:" {...register('peopleAmount', peopleAmountRegisterConfig)} invalid={errors.peopleAmount} />
-					<InputRef className={styles.roomsAmount} type="number" min="1" label="Ilość pokoi:" {...register('roomsAmount', roomsAmountRegisterConfig)} invalid={errors.roomsAmount} />
-					<InputRef className={styles.contact} type="text" label="Email lub telefon:" {...register('contact', contactRegisterConfig)} invalid={errors.contact} />
-					<InputRef className={styles.checkInAt} type="date" label="Przyjazd:" {...register('checkInAt', checkInAtRegisterConfig)} invalid={errors.checkInAt} />
-					<InputRef className={styles.checkOutAt} type="date" label="Wymeldowanie:" {...register('checkOutAt', checkOutAtRegisterConfig)} invalid={errors.checkOutAt} />
+					<Input className={styles.peopleAmount} type="number" min="1" label="Ilość osób:" {...register('peopleAmount', peopleAmountRegisterConfig)} invalid={errors.peopleAmount} />
+					<Input className={styles.roomsAmount} type="number" min="1" label="Ilość pokoi:" {...register('roomsAmount', roomsAmountRegisterConfig)} invalid={errors.roomsAmount} />
+					<Input className={styles.contact} type="text" label="Email lub telefon:" {...register('contact', contactRegisterConfig)} invalid={errors.contact} />
+					<Input className={styles.checkInAt} type="date" label="Przyjazd:" {...register('checkInAt', checkInAtRegisterConfig)} invalid={errors.checkInAt} />
+					<Input className={styles.checkOutAt} type="date" label="Wymeldowanie:" {...register('checkOutAt', checkOutAtRegisterConfig)} invalid={errors.checkOutAt} />
 				</fieldset>
 				<fieldset className={styles.button}>
 					{ <Button /> }
