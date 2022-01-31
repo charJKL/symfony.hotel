9f40bff60cd9af777c0efbac8124566660766fa7
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-31 11:16:22 +0100

    Fix `fragments\nav` href to use next.js `Link` component.

--- web/components/fragments/nav.tsx
+++ web/components/fragments/nav.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./nav.module.scss";
 
 const Nav = () : JSX.Element =>
@@ -5,28 +6,36 @@ const Nav = () : JSX.Element =>
 	return (
 		<nav className={styles.nav} >
 			<div className={styles.navDiv}>
-				<a className={styles.navLink} href="/rooms">
-					<div className={styles.navLinkMain} >Pokoje</div>
-					<div className={styles.navLinkSub} >standard &amp; ceny</div>
-				</a>
+				<Link href="/rooms">
+					<a className={styles.navLink} >
+						<div className={styles.navLinkMain} >Pokoje</div>
+						<div className={styles.navLinkSub} >standard &amp; ceny</div>
+					</a>
+				</Link>
 			</div>
 			<div className={styles.navDiv}>
-				<a className={styles.navLink} href="/conferences">
-					<div className={styles.navLinkMain} >Konferencje</div>
-					<div className={styles.navLinkSub} >infrastruktura &amp; dostępność</div>
-				</a>
+				<Link href="/conferences">
+					<a className={styles.navLink} >
+						<div className={styles.navLinkMain} >Konferencje</div>
+						<div className={styles.navLinkSub} >infrastruktura &amp; dostępność</div>
+					</a>
+				</Link>
 			</div>
 			<div className={styles.navDiv}>
-				<a className={styles.navLink} href="/recreation">
-					<div className={styles.navLinkMain}>Rekreacja</div>
-					<div className={styles.navLinkSub}>spa &amp; basen</div>
-				</a>
+				<Link href="/recreation">
+					<a className={styles.navLink} >
+						<div className={styles.navLinkMain}>Rekreacja</div>
+						<div className={styles.navLinkSub}>spa &amp; basen</div>
+					</a>
+				</Link>
 			</div>
 			<div className={styles.navDiv}>
-				<a className={styles.navLink} href="/contact">
-					<div className={styles.navLinkMain}>Kontakt</div>
-					<div className={styles.navLinkSub}>mapa &amp; telefon</div>
-				</a>
+				<Link href="/contact">
+					<a className={styles.navLink} >
+						<div className={styles.navLinkMain}>Kontakt</div>
+						<div className={styles.navLinkSub}>mapa &amp; telefon</div>
+					</a>
+				</Link>
 			</div>
 		</nav>
 	)
